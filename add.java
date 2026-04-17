4ec7e7053dadc9dfbb5cec4a13af0e768d8cfb8b
harsh090705 <157701946+harsh090705@users.noreply.github.com>
2026-04-17 11:32:50 +0530

    Add Java program for user input addition

--- add.java
+++ add.java
@@ -1,1 +1,22 @@
+import java.util.Scanner;
 
+public class AdditionExample {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Taking input from user
+        System.out.print("Enter first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = scanner.nextInt();
+
+        // Adding the numbers
+        int sum = num1 + num2;
+
+        // Displaying the result
+        System.out.println("Sum = " + sum);
+
+        scanner.close();
+    }
+}
